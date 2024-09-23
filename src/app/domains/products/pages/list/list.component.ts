868eb920ea9bfc2b325a4e4e18ab032fba6f47b6
ManuelaGaviria <manugaviria0507@gmail.com>
2024-09-23 18:05:46 -0500

    productos

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from "../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor (){
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=24',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=25',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 4',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=26',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 5',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=27',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 6',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=28',
+        creationAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string){
     console.log('estamos en el padre');
     console.log(event);
--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,19 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
+import { ProductComponent } from '../../../products/components/product/product.component';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
   @Input({required: true}) duration = 0;
   @Input({required: true}) message = '';
+  counter =signal(0);
+  counterRef: number | undefined;
   
   constructor(){
     //NO ASYNC
+    //before render
     console.log('constructor');
     console.log('-'.repeat(10));
   }
@@ -23,5 +27,42 @@ export class CounterComponent {
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+    const duration = changes['duration'];
+    if (duration && duration.currentValue != duration.previousValue) {
+      this.doSomething();
+    }
+    
+  }
+
+  ngOnInit(){
+    //after render
+    //una vez -> quedan los valores iniciales
+    //async, then, subs 
+    //fetch 
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration => ', this.duration);
+    console.log('message => ', this.message);
+    this.counterRef = window.setInterval(() => {
+      console.log('run interval')
+      this.counter.update(statePrev => statePrev + 1);
+    }, 1000)
+  }
+
+  ngAfterViewInit(){
+    //cuando los hijos ya fueron pintados
+    //after render
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef)
+  }
+
+  doSomething(){
+    console.log('change duration')
   }
 }
